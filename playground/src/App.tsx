98a94c99e68e3d901dce1e55714bf6e71d9d5273
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2025-03-13 14:34:09 +0330

    fix(theodore/playground): Adds emoji picker

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,7 +1,51 @@
+import appleEmojisData from '@emoji-mart/data/sets/15/apple.json';
+import Picker from '@emoji-mart/react';
+import type { TheodoreHandle } from '@theodore/theodore';
 import { Theodore } from '@theodore/theodore';
+import { useRef } from 'react';
+import styles from './Editor.module.scss';
+import { nativeToUnified } from './emoji';
+import './index.css';
+
+const renderEmoji = (emoji: string) => {
+  if (emoji == '') return <></>;
+  const unified = nativeToUnified(emoji);
+  const path = `/img-apple-64/${unified}.png`;
+
+  return <img src={path} width={20} height={20} />;
+};
 
 const App = () => {
-  return <Theodore />;
+  const theodoreRef = useRef<TheodoreHandle>(null);
+
+  const handleSelectEmoji = (emoji: {
+    id: string;
+    keywords: string[];
+    shortcodes: string;
+    name: string;
+    native: string;
+    unified: string;
+  }) => {
+    if (theodoreRef.current != null) {
+      theodoreRef.current.insertEmoji(emoji.native);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Picker
+        data={appleEmojisData}
+        set="apple"
+        theme="light"
+        onEmojiSelect={handleSelectEmoji}
+      />
+      <Theodore
+        className={styles.editor}
+        ref={theodoreRef}
+        renderEmoji={renderEmoji}
+      />
+    </div>
+  );
 };
 
 export default App;
