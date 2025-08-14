69ee575357ebf64b4d3e53d2f57c1a6fd38aa9c9
fatemeh karimi <fatemehkarimi.001101@gamil.com>
2025-08-14 21:26:48 +0330

    feat(theodore): Updates editor selection by moving editor caret

--- theodore/src/selection/utils.ts
+++ theodore/src/selection/utils.ts
@@ -1,4 +1,6 @@
-// this function checks if a node is an empty paragraph node
+import { Selection as EditorSelection } from '../types';
+
+// checks if a node is an empty paragraph node
 // it is empty if it has only one child, which is a <br />
 export const isEmptyParagraph = (node: Node) => {
   if (node.nodeType != Node.ELEMENT_NODE) return false;
@@ -20,10 +22,61 @@ export const getFirstNode = (node: Node) => {
   } else return firstNode;
 };
 
-// this function checks if the first child of a node is a text node,
+// checks if the first child of a node is a text node,
 // if so, returns it, otherwise returns the node
 export const getNodeOrFirstTextNode = (node: Node) => {
   const firstNode = getFirstNode(node);
   if (firstNode?.nodeType == Node.TEXT_NODE) return firstNode;
   else return node;
+};
+
+// converts dom selection to editor selection.
+export const convertDomSelectionToEditorSelection = (
+  node: Node,
+  offset: number,
+): EditorSelection => {
+  if (node.nodeType == Node.ELEMENT_NODE) {
+    if (isEmptyParagraph(node) || offset == 0) {
+      const nodeIndex = (node as HTMLElement).dataset.nodeIndex;
+      if (nodeIndex != undefined)
+        return {
+          nodeIndex: Number(nodeIndex),
+          offset,
+        };
+      else return null;
+    }
+
+    let currentNode = node.childNodes[offset - 1];
+    while (
+      currentNode.nodeType != Node.TEXT_NODE &&
+      currentNode.childNodes.length > 0
+    ) {
+      const childNode = currentNode.childNodes[0];
+      const childNodeIndex = (childNode as HTMLElement)?.dataset?.nodeIndex; // child node can be a text node
+      const nodeIndex = (currentNode as HTMLElement).dataset.nodeIndex;
+
+      if (childNodeIndex == undefined && nodeIndex != undefined) {
+        let finalOffset = offset;
+        if (childNode.nodeType == Node.TEXT_NODE)
+          finalOffset = childNode.textContent?.length ?? offset;
+
+        return {
+          nodeIndex: Number(nodeIndex),
+          offset: finalOffset,
+        };
+      }
+    }
+    return null;
+  }
+  if (node.nodeType == Node.TEXT_NODE) {
+    const parentNode = node.parentNode as HTMLElement;
+    const parentNodeIndex = parentNode?.dataset.nodeIndex;
+    if (parentNodeIndex != null) {
+      return {
+        nodeIndex: Number(parentNodeIndex),
+        offset,
+      };
+    }
+  }
+  return null;
 };
