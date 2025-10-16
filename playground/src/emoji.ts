f80c7ae52f397f6460cf5c6bdd4d148af828e29f
fatemeh karimi <fatemehkarimi.001101@gamil.com>
2025-10-16 20:51:23 +0330

    fix(theodore/playground): Fixes rendering emoji

--- playground/src/emoji.ts
+++ playground/src/emoji.ts
@@ -1,24 +1,27 @@
+const VARIATION_SELECTORS = [0xfe0e, 0xfe0f];
+
 export function nativeToUnified(emoji: string) {
   let code;
 
   if (emoji.length === 1) {
     code = emoji.charCodeAt(0).toString(16).padStart(4, '0');
   } else {
-    const pairs = [];
+    const pairs: number[] = [];
     for (let i = 0; i < emoji.length; i++) {
-      if (emoji.charCodeAt(i) >= 0xd800 && emoji.charCodeAt(i) <= 0xdbff) {
-        if (
-          emoji.charCodeAt(i + 1) >= 0xdc00 &&
-          emoji.charCodeAt(i + 1) <= 0xdfff
-        ) {
-          pairs.push(
-            (emoji.charCodeAt(i) - 0xd800) * 0x400 +
-              (emoji.charCodeAt(i + 1) - 0xdc00) +
-              0x10000,
-          );
+      const codeUnit = emoji.charCodeAt(i);
+
+      if (VARIATION_SELECTORS.includes(codeUnit)) {
+        continue;
+      }
+
+      if (codeUnit >= 0xd800 && codeUnit <= 0xdbff) {
+        const next = emoji.charCodeAt(i + 1);
+        if (next >= 0xdc00 && next <= 0xdfff) {
+          pairs.push((codeUnit - 0xd800) * 0x400 + (next - 0xdc00) + 0x10000);
+          i++;
         }
-      } else if (emoji.charCodeAt(i) < 0xd800 || emoji.charCodeAt(i) > 0xdfff) {
-        pairs.push(emoji.charCodeAt(i));
+      } else {
+        pairs.push(codeUnit);
       }
     }
 
