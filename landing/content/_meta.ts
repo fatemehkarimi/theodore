471bd4f095f1e8268e3ef8b0a0ab6475fa757cce
fatemeh karimi <fatemehkarimi.001101@gamil.com>
2026-08-23 22:45:41 +0330

    docs(theodore/landing): Adds v2 docs

---
+++ landing/content/_meta.ts
@@ -0,0 +1,26 @@
+export default {
+  index: {
+    title: 'Getting started',
+  },
+  'rendering-custom-emojis': {
+    title: 'Rendering Custom Emojis',
+  },
+  'rendering-inline-suggestions': {
+    title: 'Inline Suggestions',
+  },
+  'component-props': {
+    title: 'Component Props',
+  },
+  'use-editor-state': {
+    title: 'Editor State',
+  },
+  'theodore-ref': {
+    title: 'TheodoreRef',
+  },
+  'utility-functions': {
+    title: 'Utility Functions',
+  },
+  examples: {
+    title: 'Examples',
+  },
+};
---
+++ landing/content/rendering-inline-suggestions/_meta.ts
@@ -0,0 +1,11 @@
+export default {
+  index: {
+    title: 'Overview',
+  },
+  'suggestion-hint': {
+    title: 'Suggestion Hint',
+  },
+  'use-suggestion-hook': {
+    title: 'useSuggestion Hook',
+  },
+};
